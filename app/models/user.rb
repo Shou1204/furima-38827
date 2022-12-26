f8c1f580bbfed56693a807172a6f8f0c3ae6f46a
Shou1204 <flyingsheep0000@gmail.com>
2022-12-26 18:06:16 +0900

    バリデーションの設定、エラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[!-~]{6,}+\z/,message: "半角英数字混合での入力が必要です" }
+  validates :nickname, presence: true
+  validates :first_name_kanji, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: "漢字・ひらがな・カタカナのみ使用できます" }
+  validates :family_name_kanji, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: "漢字・ひらがな・カタカナのみ使用できます" }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}]+\z/,message: "カタカナのみ使用できます" }
+  validates :family_name_kana, presence: true, format: { with: /\A[\p{katakana}]+\z/,message: "カタカナのみ使用できます" }
+  validates :birthday, presence: true
 end
