36944c4c0c68b8378d2992a15c90bc6ec78d2b34
Shou1204 <flyingsheep0000@gmail.com>
2022-12-27 18:35:47 +0900

    ActiveHashのプルダウン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :ship_date
+  belongs_to :shipping_charge
 end
