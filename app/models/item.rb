aa0121473737b2929177fe591c535d0f0340a058
Shou1204 <flyingsheep0000@gmail.com>
2022-12-28 15:45:04 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,25 @@
 class Item < ApplicationRecord
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :prefecture
   belongs_to :ship_date
   belongs_to :shipping_charge
+  has_one_attached :image
+
+  validates :name,                presence: true
+  validates :details,             presence: true
+  validates :price,               presence: true,
+                                  numericality: { greater_than: 299, less_than: 10_000_000, message: '¥300~¥9,999,999の範囲でご入力ください' }
+  validates :category_id,         presence: true, numericality: { other_than: 1, message: 'カテゴリーを選んでください' }
+  validates :condition_id,        presence: true, numericality: { other_than: 1, message: '商品の状態を選んでください' }
+  validates :shipping_charge_id,  presence: true, numericality: { other_than: 1, message: '配送料の負担を選んでください' }
+  validates :prefecture_id,       presence: true, numericality: { other_than: 1, message: '発送元の地域を選んでください' }
+  validates :ship_date_id,        presence: true, numericality: { other_than: 1, message: '発送までの日数を選んでください' }
+  validates :user_id,             presence: true
+
+  validates :image, attached: true
+  # def was_attached?
+  #   self.image.attached?
+  # end
 end
