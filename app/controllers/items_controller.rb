ccdcecffa371300443f84e3d9e16c9c95775cd6f
Shou1204 <flyingsheep0000@gmail.com>
2022-12-28 12:10:41 +0900

    itemの値をDBに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :details, :price, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :ship_date_id).merge(user_id: current_user.id)
+  end
+
 end
