d6e7918e223d8fe41d4dab16f87cbc7c3951d897
Shou1204 <flyingsheep0000@gmail.com>
2022-12-29 19:36:34 +0900

    削除機能　destroyアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -31,6 +31,14 @@ class ItemsController < ApplicationController
       redirect_to action: :show
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if user_signed_in? && current_user == @item.user
+      if @item.destroy
+        redirect_to action: :index
+      end
     end
   end
 
