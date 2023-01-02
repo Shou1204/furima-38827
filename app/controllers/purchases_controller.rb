0742eec7ea307644e3539f1720fdc72e2c9f7ab8
Shou1204 <flyingsheep0000@gmail.com>
2023-01-02 11:49:23 +0900

    ルーティングとコントローラの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,7 +3,24 @@ class PurchasesController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-
+    @purchase_destination = PurchaseDestination.new
   end
+
+  def create
+    @purchase_destination = PurchaseDestination.new(purchase_params)
+    if @purchase_destination.valid?
+      @purchase_destination.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_destination).permit(:post, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, purchase_id: params[:item_id])
+  end
+
 
 end
