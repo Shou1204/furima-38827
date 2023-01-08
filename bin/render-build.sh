6aa07a62b62b6bdfe5993f246335a3dc0656c46d
Shou1204 <flyingsheep0000@gmail.com>
2023-01-08 22:55:09 +0900

    本番環境DBリセットのための記述

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+# bundle exec rake db:migrate #本番環境のDBリセットのため一時的にコメントアウト
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset #８行目コメントアウト、９行目追加（後で戻すこと）
