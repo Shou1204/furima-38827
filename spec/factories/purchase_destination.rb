1ba6ab31ac4bb236012b80ca80484eb93b68ef48
Shou1204 <flyingsheep0000@gmail.com>
2023-01-08 20:35:04 +0900

    ルボコップ自動修正

--- spec/factories/purchase_destination.rb
+++ spec/factories/purchase_destination.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :purchase_destination do
-    post { "123-1234" }
+    post { '123-1234' }
     prefecture_id { 2 }
     city { '品川区４丁目' }
     address { '1-1' }
-    phone_number { Faker::Number.number(digits: 10)|Faker::Number.number(digits: 11)  }
-    token {"tok_abcdefghijk00000000000000000"}
+    phone_number { Faker::Number.number(digits: 10) | Faker::Number.number(digits: 11) }
+    token { 'tok_abcdefghijk00000000000000000' }
   end
 end
--- spec/models/purchase_destination_spec.rb
+++ spec/models/purchase_destination_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe PurchaseDestination, type: :model do
     user = FactoryBot.create(:user)
     item = FactoryBot.create(:item, user_id: user.id)
     @purchase_destination = FactoryBot.build(:purchase_destination, user_id: user.id, item_id: item.id)
-    sleep 0.03 #0.03秒待機、この記述がないとエラーになる
+    sleep 0.03 # 0.03秒待機、この記述がないとエラーになる
   end
 
   describe '商品購入機能' do
@@ -28,12 +28,12 @@ RSpec.describe PurchaseDestination, type: :model do
       it 'postは「-」を含んだ正しい形式でないと購入できない' do
         @purchase_destination.post = '1234-567'
         @purchase_destination.valid?
-        expect(@purchase_destination.errors.full_messages).to include("Post 郵便番号は「ハイフン(-)」を含めた正しい形式でご入力ください")
+        expect(@purchase_destination.errors.full_messages).to include('Post 郵便番号は「ハイフン(-)」を含めた正しい形式でご入力ください')
       end
       it 'prefectureを選択していないと購入できない' do
         @purchase_destination.prefecture_id = '1'
         @purchase_destination.valid?
-        expect(@purchase_destination.errors.full_messages).to include("Prefecture 発送元の地域を選んでください")
+        expect(@purchase_destination.errors.full_messages).to include('Prefecture 発送元の地域を選んでください')
       end
       it 'cityが空だと購入できない' do
         @purchase_destination.city = ''
@@ -53,12 +53,12 @@ RSpec.describe PurchaseDestination, type: :model do
       it 'phone_numberが9桁以下だと購入できない' do
         @purchase_destination.phone_number = '123456789'
         @purchase_destination.valid?
-        expect(@purchase_destination.errors.full_messages).to include("Phone number 電話番号は10桁から11桁で入力してください")
+        expect(@purchase_destination.errors.full_messages).to include('Phone number 電話番号は10桁から11桁で入力してください')
       end
       it 'phone_numberが12桁以上だと購入できない' do
         @purchase_destination.phone_number = '123456789012'
         @purchase_destination.valid?
-        expect(@purchase_destination.errors.full_messages).to include("Phone number 電話番号は10桁から11桁で入力してください")
+        expect(@purchase_destination.errors.full_messages).to include('Phone number 電話番号は10桁から11桁で入力してください')
       end
       it 'item_idが空だと保存できない' do
         @purchase_destination.item_id = nil
@@ -71,7 +71,7 @@ RSpec.describe PurchaseDestination, type: :model do
         expect(@purchase_destination.errors.full_messages).to include("User can't be blank")
       end
       it 'tokenが空だとと保存できない' do
-        @purchase_destination.token = ""
+        @purchase_destination.token = ''
         @purchase_destination.valid?
         expect(@purchase_destination.errors.full_messages).to include("Token can't be blank")
       end
